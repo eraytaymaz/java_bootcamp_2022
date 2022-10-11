05d407ded6bd3590e9e21b4f691d5fcf72c90708
eraytaymaz <85030462+eraytaymaz@users.noreply.github.com>
2022-10-11 21:09:33 +0300

    Add files via upload

---
+++ variables/Main.java
@@ -0,0 +1,9 @@
+package variables;
+
+public class Main {
+	public static void main(String[] args) {
+		int ogrenciSayisi = 12;
+		String mesaj = "Öðrenci Sayým = ";
+		System.out.println(mesaj + ogrenciSayisi);
+	}
+}
