8fd4e6577994b4f1c431d533687e8f336ba21a9b
eraytaymaz <85030462+eraytaymaz@users.noreply.github.com>
2022-10-11 21:58:21 +0300

    Update Main.java

--- recapDemo1/Main.java
+++ recapDemo1/Main.java
@@ -10,6 +10,6 @@ public class Main {
 			enBuyuk = sayi2;
 		if(enBuyuk<sayi3)
 			enBuyuk = sayi3;
-		System.out.println("En büyük = "+enBuyuk);
+		System.out.println("En büyük sayı = "+enBuyuk);
 	}
 }
